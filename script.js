dd88f80f0b919213cff825693b39d8ef442ead5c
Edward Gargan <edwgargan@gmail.com>
2019-01-27 19:40:21 +0000

    Fix picker window layout + templatise picker HTML

--- script.js
+++ script.js
@@ -4,21 +4,28 @@ class picker {
         this.list = list;
     }
     getUl() {
-        var ul = document.createElement('ul');
-        ul.className += "picker";
+        const pickerTemplate = '<ul class="picker"><div class="window"/></ul>';
+
+        var holder = document.createElement('div');
+        holder.innerHTML = pickerTemplate;
+        
+        var picker = holder.firstChild;
+
         for (var i = 0; i < this.list.length; i++) {
             var li = document.createElement('li');
-            ul.appendChild(li);
+            picker.appendChild(li);
             li.innerHTML = this.list[i];
         }
-        return ul;
+
+        return picker;
     }
 };
 
 let eight = new picker([1,2,3,4,5,6,7,8]);
-let ten = new picker([1,2,3,4,5,6,7,8,9,10,11]);
+let sixteen = new picker([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
 
 var container = document.getElementById('container');
 
 container.appendChild(eight.getUl());
-container.appendChild(ten.getUl());
+container.appendChild(sixteen.getUl());
+container.appendChild(sixteen.getUl());

96164c8f5c82270dbc958808a40cf8801d4380e1
Edward Gargan <edwgargan@gmail.com>
2019-02-03 21:51:39 +0000

    Add list append + prepend methods to picker
    Picker needs to appear circular, so we'll need to append/prepend chunks
    when picker bounds are reached.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-class picker {
+class rolypicker {
 
     constructor(list) {
         this.list = list;
@@ -7,19 +7,51 @@ class picker {
         this.cellSize = 40;
     }
 
+    // Get index of cell 'closest' to current scroll location
     getNearestCell() {
-        // Get index of cell 'closest' to current scroll level
         return Math.round(this.picker.scrollTop / this.cellSize);
     }
 
     scrollToCell(index) {
         var scrollTo = index * this.cellSize;
 
-        // todo: find more supported way of animating scroll
+        // todo: find more supported way of animating scroll - json 'scrollToOptions' fairly new
         this.picker.scrollTo({
             top: scrollTo,
             behavior: 'smooth'
         });
+    }
+
+    addHead() {
+        if (this.picker) {
+
+            var currScroll = this.picker.scrollTop;
+
+            // todo: create prebuilt list of nodes so we can insert in a single move
+            for (var i = this.list.length - 1; i >= 0; i--) {
+                var li = document.createElement('li');
+                this.picker.insertBefore(li, this.picker.childNodes[1]);
+                li.innerHTML = this.list[i];
+            }
+
+            // Prepending new elements when scrollTop's at 0 for some reason
+            // automatically scrolls to container top, to maintain scrollTop == 0.
+            // Account for this by scrolling to bottom of new cells.
+            if (currScroll == 0) {
+                this.picker.scrollTop = this.list.length * this.cellSize;
+            }
+        }
+    }
+
+    addTail() {
+        if (this.picker) {
+            // todo: create prebuilt list of nodes so we can insert in a single move
+            for (var i = 0; i < this.list.length; i++) {
+                var li = document.createElement('li');
+                this.picker.appendChild(li);
+                li.innerHTML = this.list[i];
+            }
+        }
     }
 
     getUl() {
@@ -40,7 +72,7 @@ class picker {
             window.clearTimeout(isScrolling);
             isScrolling = setTimeout(function() {
                 classRef.scrollToCell(classRef.getNearestCell());
-            }, 100);
+            }, 80);
         };
 
         for (var i = 0; i < this.list.length; i++) {
@@ -58,16 +90,18 @@ class picker {
 
         return picker;
     }
-
 };
 
 function getId(id) {
     return document.getElementById(id);
 }
 
-let eight = new picker([1,2,3,4,5,6,7,8]);
-let sixteen = new picker([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
+let eight = new rolypicker([1,2,3,4,5,6,7,8]);
+let sixteen = new rolypicker([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
 
 var container = getId('container');
 container.appendChild(eight.getUl());
 container.appendChild(sixteen.getUl());
+
+eight.addTail();
+eight.addHead();
